73a0659821fe3972d46ca710def4329dcf01715d
William{at}Offbeat.Space <william@offbeat.space>
2017-02-11 18:50:24 -0700

    completes static (stateless) build of app

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import uuid from 'uuid';
 
-const DATA = [ 5, new Date(), [15,12,18] ];
+const DATA = [ 5, [15,12,18] ];
+const dotw = ['Su','Mo','Tu','We','Th','Fr','Sa'];
 
 function zeroPadSmallerThanTen(val) {
   if (val < 10) { return ('0'+val); }
   return val;
 }
 
+function calculateEndDate(pgs, ppd, end) {
+  var duration = Math.ceil(pgs/ppd);
+  end.setDate(end.getDate() + duration);
+  return {
+    d: end,
+    month: end.getMonth() + 1,
+    date: end.getDate(),
+    day: dotw[end.getDay()]
+  };
+}
+
+
 class BookProgressChartGenerator extends React.Component {
  render() {
    return (
     <div>
       <PagesPerDayField ppd={this.props.data[0]} />
-      <ProgressChart lastDate={this.props.data[1]} chapterPages={this.props.data[2]} />
+      <ProgressChart chapterPages={this.props.data[1]} ppd={this.props.data[0]} />
     </div>
    );
  }
@@ -24,24 +38,31 @@ class PagesPerDayField extends React.Component {
     let ppdVal = zeroPadSmallerThanTen(this.props.ppd);
     return (
 			<section className="app-ppd_wrapper">
-				<input className="app-ppd_input" value={ppdVal} name="ppd" type="number" step="1" min="1" />
-				<label className="app-ppd_label" for="ppd" title="Pages per Day">P/D</label> 
+				<input className="app-ppd_input" defaultValue={ppdVal} name="ppd" type="number" step="1" min="1" />
+				<label className="app-ppd_label" htmlFor="ppd" title="Pages per Day">P/D</label> 
 				<p className="app-ppd_helper">Enter number of pages read per day</p>
 			</section>
     );
   }
 }
+
 class ProgressChart extends React.Component {
   render() {
-    let chartRows = [];
+
+    let chartRows = [],
+        rollingDate = new Date(); 
 
     for (let i = 0; i < this.props.chapterPages.length; i++) {
-      if (i === 0) {
-        chartRows.push(<FirstChapterRow pages={this.props.chapterPages[i]} />);
-        continue;
-      }
-      chartRows.push(<AddedChapterRow pages={this.props.chapterPages[i]} />);
+
+      let lastDate = calculateEndDate(this.props.chapterPages[i],this.props.ppd,rollingDate),
+          projectedDate = lastDate.month + '/' + lastDate.date;
+
+      chartRows.push(<ChapterRow isFirst={i === 0} key={'ch_'+uuid()} pages={this.props.chapterPages[i]} projected={projectedDate} />);
+      rollingDate = lastDate.d;
+
     }
+
+    console.log(chartRows);
 
     return (
 			<section className="app-chart_wrapper cf">
@@ -65,42 +86,28 @@ class ProgressChart extends React.Component {
     );
   }
 }
-class FirstChapterRow extends React.Component {
+
+class ChapterRow extends React.Component {
   render() {
-    let pageCount = zeroPadSmallerThanTen(this.props.pages);
-    return (
-	  	<tr>
-				<td><span className="app-chart_chapNum"></span></td>
-	  		<td className=""><input className="app_chart_field pagesInChapter" value={pageCount} type="number" max="999" min="1" /></td>
-				<td className="null"></td>
-				<td className="null"></td>
-				<td className="null app-chart-projected"></td>
-				<td className="null"></td>
-				<td className="null large"></td>
-				<td className="null large"></td>
-			</tr>  
-    );
-  }
-}
-class AddedChapterRow extends React.Component {
-  render() {
-    let pageCount = zeroPadSmallerThanTen(this.props.pages);
+    let pageCount = zeroPadSmallerThanTen(this.props.pages),
+        rowRemove = (this.props.isFirst) ? null : <a className="app-chart-remove" href="javascript:void(0);" tabIndex="-1">-</a>;
     return (
 		  <tr>
 				<td><span className="app-chart_chapNum"></span></td>
-				<td className=""><input className="app_chart_field pagesInChapter" value={pageCount} type="number" max="999" min="1" /></td>
+				<td className=""><input className="app_chart_field pagesInChapter" defaultValue={pageCount} type="number" max="999" min="1" /></td>
 				<td className="null"></td>
 				<td className="null"></td>
-				<td className="null app-chart-projected"></td>
+				<td className="app-chart-projected">{this.props.projected}</td>
 				<td className="null"></td>
 				<td className="null large"></td>
 				<td className="null large">
-				  <a className="app-chart-remove" href="javascript:void(0);" tabindex="-1">-</a>
+				  {rowRemove}
         </td>
 			</tr>
     );
   }
 }
+
 class Toolbar extends React.Component {
   render() {
     return (
